1304fb969456356ea2113cff11a81a167fecb116
jiweiling <weiling.ji@shanbay.com>
2019-03-04 16:21:33 +0800

    feat: admin resource

--- my_blog/settings/development.py
+++ my_blog/settings/development.py
@@ -1,1 +1,13 @@
 from .default import *
+import os
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'HOST': os.environ["DB_HOST"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"]
+    }
+}
